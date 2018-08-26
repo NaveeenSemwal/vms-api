94e657e667f5f1a6443661f08526deed7c653a86
naveen semwal <naveenrhtdm4ever@gmail.com>
2018-08-26 21:07:24 +0530

    Comment replace hardcoded connection details

--- NodeJS/db.js
+++ NodeJS/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const server = 'localhost:27017'; // REPLACE WITH YOUR DB SERVER
 const database = 'EMS';      // REPLACE WITH YOUR DB NAME
 
+// need to replace this hard coding server and port.
 mongoose.connect("mongodb://localhost:27017/EMS", { useNewUrlParser: true }, (err) => {
 
     if (!err) {
